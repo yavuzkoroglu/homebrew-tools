aa00c0354756a44c74ac7905bfe13a280a29bf34
yavuzkoroglu <yavuz1990@gmail.com>
2026-09-16 14:21:35 +0300

    Updated qrcreate from 2.1.0 to 2.1.1

--- Formula/qrcreate.rb
+++ Formula/qrcreate.rb
@@ -1,8 +1,8 @@
 class Qrcreate < Formula
   desc "A simple Bash script that creates a PDF qrcode of data given from standard input, by default saving it to 'qr.pdf' and converting it to clickable a hyperlink whenever possible."
   homepage "https://github.com/yavuzkoroglu/qrcreate"
-  url "https://github.com/yavuzkoroglu/qrcreate/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "c7ddc48ba8b5f559e609c9452bf85c2b7ec08de7932a44804bcc020b659ef0c8"
+  url "https://github.com/yavuzkoroglu/qrcreate/archive/refs/tags/v2.1.1.tar.gz"
+  sha256 "cf60f1ff89114b7454b79fb669c39fb09e0143ba194c2242b3f0a2f7f69d6855"
   license "MIT"
 
   deny_network_access!
